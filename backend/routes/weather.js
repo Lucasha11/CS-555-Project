927d8decd0f421ebde730bc57fa13d46b5ebc4df
user <user@DESKTOP-H2LMAI21>
2025-10-23 14:56:17 -0400

    Added template and backend forecast functions

---
+++ backend/routes/weather.js
@@ -0,0 +1,23 @@
+import {Router} from 'express';
+const router = Router();
+import { weatherData } from '../data/index.js';
+
+
+
+
+router
+  .route('/forecast')
+  .get(async (req, res) => {
+    try {
+      if (!req.body.location || typeof req.body.location !== 'string'){
+        throw new Error('Location must be a valid string')
+      }
+      const weatherList = await weatherData.getDailyForecast(req.body.location);
+      return res.json(weatherList);
+      } catch (e) {
+      console.log(e);
+      return res.status(500).send(e);
+      }
+    });
+
+export default router;
