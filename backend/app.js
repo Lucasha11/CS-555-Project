5cdca92d68b1ea7b41fb19ba436d3c3aebf76f2b
4rrgon <129262318+4rrgon@users.noreply.github.com>
2025-12-01 16:43:01 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,15 @@
 import express from 'express';
+import cors from 'cors'; 
 const app = express();
 import configRoutes from './routes/index.js';
 
-
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 configRoutes(app);
 
-
 app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
-
-
